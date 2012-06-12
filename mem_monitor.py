801e04645dd4dae72c01ef42dd5fcdb34c5b3377
root <root@stunning-apps.com>
2012-06-12 12:46:08 +0200

    Monitors privvmpages for full memory, and sends email alerts.

---
+++ mem_monitor.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import re
+import smtplib
+from email.mime.text import MIMEText
+from socket import gethostname
+
+warn_ratio = 0.90
+email_to = ["per@stunning.se", "johan@stunning.se"]
+email_from = "root@stunning-apps.com"
+
+once_filename = "/tmp/mem_monitor-once.json"
+
+def check():
+	pattern = re.compile("""privvmpages""" + 
+		"""\s+(?P<held>\d+)""" +
+		"""\s+(?P<maxheld>\d+)""" +
+		"""\s+(?P<barrier>\d+)""" +
+		"""\s+(?P<limit>\d+)""" +
+		"""\s+(?P<failcnt>\d+)""")
+	with open("/proc/user_beancounters", "rb") as data:
+		match = pattern.search(data.read())
+		p = dict((k, int(v)) for k, v in match.groupdict().items())
+		ratio = p["maxheld"] / float(p["limit"])
+		if p["failcnt"]:
+			alert("fail", "Memory limit breached")
+		elif ratio >= warn_ratio:
+			alert("warn", "Memory %.0f%% full" % (ratio*100))
+		elif p["maxheld"] >= p["barrier"]:
+			alert("warn", "memory barier brached")
+		else:
+			alert("ok")
+	
+def alert(type, message=None):
+	try:
+		with open(once_filename, "rb") as once_file:
+			once = once_file.read()
+	except:
+		once = "ok"
+	if type == once:
+		return
+	
+	if type in ["fail", "warn"]:
+		email(message)
+		
+	with open(once_filename, "wb") as once_file:
+		once_file.write(type)
+	
+def email(message):
+	msg = MIMEText("\n".join([
+		message,
+		"",
+		"Host: " + gethostname()]))
+	
+	msg['Subject'] = "STUNNIMP: " + message
+	msg['From'] = email_from
+	msg['To'] = ', '.join(email_to)
+	
+	s = smtplib.SMTP('localhost')
+	s.sendmail(email_from, email_to, msg.as_string())
+	s.quit()
+
+
+check()
